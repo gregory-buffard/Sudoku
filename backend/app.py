b795493bddfd1cb95703f867ba15ce307cf28c9d
AnorakDWise <gregory442005@gmail.com>
2024-01-21 09:43:16 +0100

    Testing Tesseract OCR, will build custom model...

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,31 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, flash, redirect, url_for
 from flask_cors import CORS
+from werkzeug.utils import secure_filename
 from solver import solve_sudoku
+from image_processing import process_and_extract_sudoku
+import os
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/solve-image', methods=['POST'])
+def solve_from_image():
+    if 'image' not in request.files:
+        return jsonify({"error": "Aucun fichier."}), 400
+    file = request.files['image']
+    if file.filename == '':
+        return jsonify({"error": "Aucun fichier selectioné."}), 400
+    filename = secure_filename(file.filename)
+    file_path = os.path.join('./images', filename)
+    file.save(file_path)
+    try:
+        sudoku_board = process_and_extract_sudoku(file_path)
+        if solve_sudoku(sudoku_board):
+            return jsonify({"solution": sudoku_board})
+        else:
+            return jsonify({"solution": sudoku_board})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @app.route('/solve', methods=['POST'])
 def solve():
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,6 +8,7 @@ const Home = () => {
   const initGrid = Array(9).fill(null).map(() => Array(9).fill(0)),
       [grid, setGrid] = useState(initGrid),
       [lightSwitch, setLightSwitch] = useState<boolean>(true),
+      [selectedFile, setSelectedFile] = useState<File | null>(null),
       [solving, setSolving] = useState<boolean>(false);
 
   const inputChange = (row: number, col: number, value: string): void => {
@@ -36,6 +37,47 @@ const Home = () => {
     }
   }
 
+  const fileChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0];
+      if (file && file.type === 'image/png' || file.type === 'image/jpeg' || file.type === 'image/jpg') {
+        setSelectedFile(file);
+      } else {
+        alert('Veuillez sélectionner une image au format png ou jpg');
+        setSelectedFile(null);
+      }
+    }
+  }
+
+  const upload = async (): Promise<void> => {
+    if (!selectedFile) {
+      alert('Veuillez sélectionner une image');
+      return;
+    }
+
+    setSolving(true);
+    const formData = new FormData();
+    formData.append('image', selectedFile);
+
+    try {
+      const res = await axios.post('http://localhost:5000/solve-image', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+        setGrid(res.data.solution);
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        const error = err.response?.data.error || 'Une erreur est survenue';
+        alert(error);
+      } else {
+        alert('Une erreur est survenue');
+      }
+    } finally {
+        setSolving(false);
+    }
+  }
+
   return (
     <main className={`${lightSwitch ? '' : 'dark'} m-auto flex justify-center items-center display-mode-transition`}>
       <LightSwitch status={lightSwitch} setStatus={setLightSwitch} />
@@ -54,10 +96,15 @@ const Home = () => {
                     className={'bg-blue-400 dark:bg-blue-800 text-blue-200 dark:text-blue-400 text-[2vh] px-[1.5vh] py-[1vh] rounded-[1.25vh] display-mode-transition font-medium'}>
               {solving ? 'Resolution...' : 'Resoudre'}
             </button>
-            {solving ? <></> : <button onClick={() => setGrid(initGrid)} disabled={solving}
-                                       className={'bg-red-400 dark:bg-red-800 text-red-200 dark:text-red-400 text-[2vh] px-[1.5vh] py-[1vh] rounded-[1.25vh] display-mode-transition font-medium'}>
-              Effacer
-            </button>}
+            {solving ? <></> :<>
+                <button onClick={() => setGrid(initGrid)} disabled={solving}
+                                       className={'bg-red-400 dark:bg-red-800 text-red-200 dark:text-red-400 text-[2vh] px-[1.5vh] py-[1vh] rounded-[1.25vh] display-mode-transition font-medium'}>Effacer</button>
+                <div>
+                  <input type={'file'} onChange={fileChange} />
+                  <button onClick={upload}>Upload and solve</button>
+                </div>
+            </>
+            }
           </div>
         </div>
       </div>
